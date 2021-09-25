08514b110cac9549db1ead1fe1de4de51df8b99d
zorsen117 <zorsen117@gmail.com>
2021-09-24 19:07:16 -0500

    Ejercicios de la clase 24/09/21

---
+++ ejercicios/24_09_21-ejercicio1.js
@@ -0,0 +1,14 @@
+let alumnos = [
+    { nombre: "Pepe", calificacion: 10, yaPago: false },
+    { nombre: "María", calificacion: "9" },
+    { nombre: "Rosa", calificacion: 6, yaPago: true },
+    { nombre: "Hernan", calificacion: 9, yaPago: false },
+    { nombre: "Jimena", calificacion: 5, yaPago: true },
+    { nombre: "Teresa", nuncaSePresento: true }
+];
+
+// 1. Crear una función que devuelva los nombres de todos los alumnos
+// 2. Crear una función que devuelva los nombres de todos los alumnos que aprobaron (la calificación mínima para aprobar es 6)
+// 3. Crear una función que devuelva los nombres de todos los alumnos que falta por pagar
+// 4. Crear una función que devuelva los nombres de todos los alumnos que no se presentaron
+// 5. Ejecutar cada función e imprimir los resultados en una lista vertical (un nombre en cada línea)
