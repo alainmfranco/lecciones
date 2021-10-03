e370698c965126735d858a3081f3e3bc70ac6781
zorsen117 <zorsen117@gmail.com>
2021-10-02 20:01:05 -0500

    Ejercicios para el primero de octubre de 2021

---
+++ ejercicios/01_10_21-ejercicio2/srcipt.js
@@ -0,0 +1,7 @@
+// Crear el código para rellenar la tabla con los datos de la forma
+
+// Validaciones:
+// Nombre debe de ser texto y no estar vacío
+// Edad tiene que se mayor a 15 años
+// Sexo debe ser masculino o femenino
+// Carrera no debe de estar vacío
